0df88156cb946b8c93187b510e4f3f81045b7ea4
sooti <sooti85@gmail.com>
2018-05-02 12:22:51 +0000

    d855: this is aosp

--- aosp_d855.mk
+++ aosp_d855.mk
@@ -20,12 +20,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from d855 device
 $(call inherit-product, device/lge/d855/device.mk)
 
-# Inherit some common LiquidRemix stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# AOSP Packages
+PRODUCT_PACKAGES += \
+    Launcher3 \
+    messaging \
+    Terminal
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := d855
-PRODUCT_NAME := liquid_d855
+PRODUCT_NAME := aosp_d855
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-D855
 PRODUCT_MANUFACTURER := LGE
